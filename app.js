d0023de9a22109502043190409341ae33154a38d
Priti Yadav <pritiyadav888@gmail.com>
2021-02-11 12:35:38 -0600

    changes to add a logo

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
 let app = express();
+//Adding background image
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,15 +33,23 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use('public', express.static(__dirname + 'public'));
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  //render the error page
   res.status(err.status || 500);
   res.render('error', {title: 'Error'});
 });
+
+// app.get("/about", function(req, res){
+//   res.render("background", {images:images});
+// });
+
+
 
 module.exports = app;
